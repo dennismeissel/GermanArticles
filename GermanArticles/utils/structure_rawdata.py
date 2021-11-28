97c61b2dab9022ce281b657772a483c96c68ba6b
Dennis <meissel@mail.de>
2021-11-28 17:42:59 +0100

    Finished the algorithm and added the resulting csv

--- GermanArticles/utils/structure_rawdata.py
+++ GermanArticles/utils/structure_rawdata.py
@@ -14,31 +14,36 @@ def check_line(_line):
         entries = _line.split('\t')
         if entries[0] != entries[1]:
             # It is not a main form of a word
-            return 0
+            return None
         word_info = entries[2].split(':')
         if word_info[0] != 'SUB':
             # not a noun
-            return 0
+            return None
         if word_info[1] != 'NOM':
             # Not 'Nominativ'
-            return 0
+            return None
         if word_info[2] != 'SIN':
             # Not singular
-            return 0
+            return None
         if word_info[3] == 'MAS':
             return 1, entries[0]
         if word_info[3] == 'FEM':
             return 2, entries[0]
         if word_info[3] == 'NEU':
             return 3, entries[0]
-    return 0
+    return None
 
 
-path = input("Input the path to the words.dump file\r\n")
-path = path.strip('"')
-lines = read_lines(path)
+path_input = input("Enter the path to the words.dump file\n")
+path_output = input("Enter the path to the output file\n")
 
-for line in lines:
-    result = check_line(line)
-    if result != 0:
-        print(result)
+path_input = path_input.strip('"')
+path_output = path_output.strip('"')
+lines = read_lines(path_input)
+
+with open(path_output, mode='w', encoding="utf8") as file:
+    for line in lines:
+        result = check_line(line)
+        if result is not None:
+            file.write(result[1] + ";" + str(result[0]) + "\n")
+
